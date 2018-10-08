9f531b2fb7f26d74c1539000f16cd66268e4932a
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-10-08 11:03:44 +0100

    Initial commit

---
+++ Assets/Scenes/GameSelectionScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSelectionScreen : MonoBehaviour 
+{
+	[SerializeField] private GameObject titleScreen;
+	[SerializeField] private GameObject title;
+	[SerializeField] private GameObject easy;
+	[SerializeField] private GameObject normal;
+	[SerializeField] private GameObject hard;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine( Sequence() );
+	}
+	
+	private IEnumerator Sequence()
+	{
+		//yield return new WaitForSeconds( 1.0f );
+
+		title.SetActive( true );
+
+		yield return new WaitForSeconds( 1.0f );
+		
+		titleScreen.SetActive( false );
+	}
+}
---
+++ Assets/Scripts/DropBounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBounce : MonoBehaviour {
+
+	// Use this for initialization
+
+	[SerializeField] private Vector3 target;
+	void Start () 
+	{
+		  iTween.MoveTo(gameObject,iTween.Hash(
+			  "position", target,
+			  "islocal", true,
+			  "easetype",iTween.EaseType.easeOutBounce,
+			  "time",1.0f)
+		  );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
